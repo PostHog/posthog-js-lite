1f400bea45c65e3217b5b84a7445626360229eb9
Ben White <ben@benjackwhite.co.uk>
2022-07-18 12:02:09 +0200

    Added wrapper for Node to use shared storage for queue

--- posthog-node/src/posthog-node.ts
+++ posthog-node/src/posthog-node.ts
@@ -12,18 +12,24 @@ const version = '2.0.0-alpha'
 
 export interface PostHogNodejsOptions extends PosthogCoreOptions {}
 
+const SHARED_PERSISTENCE_PROPERTIES = [PostHogPersistedProperty.Queue]
+
 export class PostHogNodejs extends PostHogCore {
+  _sharedStorage: { [key: string]: any | undefined } = {}
   _memoryStorage: { [key: string]: any | undefined } = {}
 
-  constructor(apiKey: string, options: PostHogNodejsOptions) {
+  constructor(apiKey: string, options: PostHogNodejsOptions, globalStorage: { [key: string]: any | undefined }) {
     super(apiKey, options)
+    this._sharedStorage = globalStorage
   }
 
   getPersistedProperty(key: PostHogPersistedProperty): any | undefined {
-    return this._memoryStorage[key]
+    const storage = SHARED_PERSISTENCE_PROPERTIES.includes(key) ? this._sharedStorage : this._memoryStorage
+    return storage[key]
   }
   setPersistedProperty(key: PostHogPersistedProperty, value: any | null): void {
-    this._memoryStorage[key] = value !== null ? value : undefined
+    const storage = SHARED_PERSISTENCE_PROPERTIES.includes(key) ? this._sharedStorage : this._memoryStorage
+    storage[key] = value !== null ? value : undefined
   }
 
   fetch(url: string, options: PostHogFetchOptions): Promise<PostHogFetchResponse> {
@@ -43,10 +49,12 @@ export class PostHogNodejs extends PostHogCore {
 
 // The actual exported Nodejs API.
 export default class PostHogNodejsGlobal {
+  _sharedStorage: { [key: string]: any | undefined } = {}
+
   constructor(private apiKey: string, private options: PostHogNodejsOptions) {}
 
   user(distinctId: string): PostHogNodejs {
-    const client = new PostHogNodejs(this.apiKey, this.options)
+    const client = new PostHogNodejs(this.apiKey, this.options, this._sharedStorage)
     client.setPersistedProperty(PostHogPersistedProperty.DistinctId, distinctId)
     return client
   }
