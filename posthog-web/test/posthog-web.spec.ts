143c9340adc1c21b07c36144115e2b36f077cc3e
Ben White <ben@benjackwhite.co.uk>
2022-07-19 17:00:18 +0200

    Tests

--- posthog-web/test/posthog-web.spec.ts
+++ posthog-web/test/posthog-web.spec.ts
@@ -9,8 +9,7 @@ describe('PosthogWeb', () => {
   jest.useFakeTimers()
 
   beforeEach(() => {
-    // setupDom()
-    global.window.fetch = fetch = jest.fn(() =>
+    (global as any).window.fetch = fetch = jest.fn(() =>
       Promise.resolve({
         status: 200,
         json: () => Promise.resolve({ status: 'ok' }),
