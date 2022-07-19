225eb7bde208345cea97ff21a0f5d822708707cb
Ben White <ben@benjackwhite.co.uk>
2022-07-19 16:37:23 +0200

    Fix tests

--- posthog-core/test/test-utils/test-utils.ts
+++ posthog-core/test/test-utils/test-utils.ts
@@ -6,7 +6,7 @@ export const wait = async (t: number) => {
 
 export const waitForPromises = async () => {
   jest.useRealTimers()
-  await new Promise((resolve) => (setInterval(resolve, 1) as any))
+  await new Promise((resolve) => (setTimeout(resolve, 1) as any))
   jest.useFakeTimers()
 }
 
