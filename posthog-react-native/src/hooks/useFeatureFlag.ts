c052b3c1de3b792b2332249ce24ca9e23057cf32
Mo Sattler <hi@mosattler.com>
2022-11-21 12:24:19 +0100

    Update useFeatureFlag.ts
    Conditional hooks is against the rules of hooks and will cause errors. Hence this change.

--- posthog-react-native/src/hooks/useFeatureFlag.ts
+++ posthog-react-native/src/hooks/useFeatureFlag.ts
@@ -6,15 +6,13 @@ export function useFeatureFlag(flag: string): string | boolean | undefined {
 
   const [featureFlag, setFeatureFlag] = useState<boolean | string | undefined>(posthog?.getFeatureFlag(flag))
 
-  if (!posthog) {
-    return featureFlag
-  }
-
   useEffect(() => {
-    setFeatureFlag(posthog.getFeatureFlag(flag))
-    return posthog.onFeatureFlags(() => {
+    if (posthog) {
       setFeatureFlag(posthog.getFeatureFlag(flag))
-    })
+      return posthog.onFeatureFlags(() => {
+        setFeatureFlag(posthog.getFeatureFlag(flag))
+      })
+    }
   }, [posthog, flag])
 
   return featureFlag
