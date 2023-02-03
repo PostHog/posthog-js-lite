81c0fa5af4e7bee41ba6bd817b740ee76b0a7f32
Ben White <ben@posthog.com>
2023-02-03 09:00:02 +0100

    feat: Export the `getAppProperties` function to allow full customisation

--- posthog-react-native/test/init.spec.ts
+++ posthog-react-native/test/init.spec.ts
@@ -1,4 +1,4 @@
-import { PostHog } from '../src/posthog-rn'
+import { PostHog, getAppProperties } from '../index'
 
 let posthog: PostHog
 
@@ -15,5 +15,44 @@ describe('PostHog React Native', () => {
     jest.runOnlyPendingTimers()
     expect(posthog.getAnonymousId()).toEqual('bar')
     expect(posthog.getDistinctId()).toEqual('bar')
+  })
+
+  it('should allow customising of native app properties', () => {
+    posthog = new PostHog('test-token', { customAppProperties: { $app_name: 'mock' } })
+
+    expect(posthog.getCommonEventProperties()).toEqual({
+      $active_feature_flags: undefined,
+      $app_name: 'mock',
+      $device_type: 'ios',
+      $lib: 'posthog-react-native',
+      $lib_version: expect.any(String),
+      $screen_height: expect.any(Number),
+      $screen_width: expect.any(Number),
+    })
+
+    const properties = getAppProperties()
+    properties.$app_name = 'customised!'
+    delete properties.$device_name
+
+    posthog = new PostHog('test-token', { customAppProperties: properties })
+
+    expect(posthog.getCommonEventProperties()).toEqual({
+      $active_feature_flags: undefined,
+      $device_type: 'ios',
+      $lib: 'posthog-react-native',
+      $lib_version: expect.any(String),
+      $screen_height: expect.any(Number),
+      $screen_width: expect.any(Number),
+      $app_build: 'mock',
+      $app_name: 'customised!', // changed
+      $app_namespace: 'mock',
+      $app_version: 'mock',
+      $device_manufacturer: 'mock',
+      // $device_name: 'mock', (deleted)
+      $os_name: 'mock',
+      $os_version: 'mock',
+      $locale: 'mock',
+      $timezone: 'mock',
+    })
   })
 })
