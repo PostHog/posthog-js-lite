5daa793283fb7e2ad12bccd6c0273778be563ded
Jonathan <Jonatthu@gmail.com>
2023-09-25 16:04:28 -0700

    Update useFeatureFlag.ts
    Enabling feature flags with payload, this should be essential to have...

--- posthog-react-native/src/hooks/useFeatureFlag.ts
+++ posthog-react-native/src/hooks/useFeatureFlag.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { usePostHog } from './usePostHog'
+import { JsonType } from 'posthog-core/src'
 
 export function useFeatureFlag(flag: string): string | boolean | undefined {
   const posthog = usePostHog()
@@ -17,4 +18,25 @@ export function useFeatureFlag(flag: string): string | boolean | undefined {
   }, [posthog, flag])
 
   return featureFlag
+}
+
+export type FeatureFlagWithPayload = [boolean | string | undefined, JsonType | undefined];
+
+export function useFeatureFlagWithPayload(flag: string): FeatureFlagWithPayload {
+  const posthog = usePostHog()
+
+  const [featureFlag, setFeatureFlag] = useState<FeatureFlagWithPayload>([undefined, undefined])
+  
+  useEffect(() => {
+    if (!posthog) {
+      return
+    }
+
+    setFeatureFlag([posthog.getFeatureFlag(flag), posthog.getFeatureFlagPayload(flag)])
+    return posthog.onFeatureFlags(() => {
+      setFeatureFlag([posthog.getFeatureFlag(flag), posthog.getFeatureFlagPayload(flag)])
+    })
+  }, [posthog, flag])
+
+  return featureFlag;
 }
