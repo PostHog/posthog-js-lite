dcd429a743de6ad45181c24e13e30285a5070a59
Jonathan <Jonatthu@gmail.com>
2023-10-05 23:08:14 -0700

    Added useFeatureFlagWithPayload for react native (#111)
    Co-authored-by: Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>

--- posthog-react-native/CHANGELOG.md
+++ posthog-react-native/CHANGELOG.md
@@ -1,3 +1,7 @@
+# Next
+
+1. Added new `const [flag, payload] = useFeatureFlagWithPayload('my-flag-name')` hook that returns the flag result and it's payload if it has one.
+
 # 2.7.1 - 2023-05-25
 
 1. The `$screen_name` property will be registered for all events whenever `screen` is called
--- posthog-react-native/src/hooks/useFeatureFlag.ts
+++ posthog-react-native/src/hooks/useFeatureFlag.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { usePostHog } from './usePostHog'
+import { JsonType } from 'posthog-core/src'
 
 export function useFeatureFlag(flag: string): string | boolean | undefined {
   const posthog = usePostHog()
@@ -13,6 +14,27 @@ export function useFeatureFlag(flag: string): string | boolean | undefined {
     setFeatureFlag(posthog.getFeatureFlag(flag))
     return posthog.onFeatureFlags(() => {
       setFeatureFlag(posthog.getFeatureFlag(flag))
+    })
+  }, [posthog, flag])
+
+  return featureFlag
+}
+
+export type FeatureFlagWithPayload = [boolean | string | undefined, JsonType | undefined]
+
+export function useFeatureFlagWithPayload(flag: string): FeatureFlagWithPayload {
+  const posthog = usePostHog()
+
+  const [featureFlag, setFeatureFlag] = useState<FeatureFlagWithPayload>([undefined, undefined])
+
+  useEffect(() => {
+    if (!posthog) {
+      return
+    }
+
+    setFeatureFlag([posthog.getFeatureFlag(flag), posthog.getFeatureFlagPayload(flag)])
+    return posthog.onFeatureFlags(() => {
+      setFeatureFlag([posthog.getFeatureFlag(flag), posthog.getFeatureFlagPayload(flag)])
     })
   }, [posthog, flag])
 
