cc0a442ea20a81d21ecc8cd2be9f8e41465e6b54
Ben White <ben@posthog.com>
2024-01-02 14:56:56 +0100

    Removed

--- examples/example-node/server.ts
+++ examples/example-node/server.ts
@@ -27,10 +27,6 @@ posthog.debug()
 
 Sentry.init({
   dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
   integrations: [new PostHogSentryIntegration(posthog)],
 })
 
