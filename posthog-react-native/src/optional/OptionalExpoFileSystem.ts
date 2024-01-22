26ab757bc959c1df00245e0fdb2aa58375ea8bd4
Manoel Aranda Neto <marandaneto@gmail.com>
2024-01-22 17:00:15 +0100

    add comment

--- posthog-react-native/src/optional/OptionalExpoFileSystem.ts
+++ posthog-react-native/src/optional/OptionalExpoFileSystem.ts
@@ -3,5 +3,5 @@ import type ExpoFileSystem from 'expo-file-system'
 export let OptionalExpoFileSystem: typeof ExpoFileSystem | undefined = undefined
 
 try {
-  OptionalExpoFileSystem = require('expo-file-system')
+  OptionalExpoFileSystem = require('expo-file-system') // No Web support
 } catch (e) {}
--- posthog-react-native/src/optional/OptionalReactNativeDeviceInfo.ts
+++ posthog-react-native/src/optional/OptionalReactNativeDeviceInfo.ts
@@ -3,5 +3,5 @@ import type ReactNativeDeviceInfo from 'react-native-device-info'
 export let OptionalReactNativeDeviceInfo: typeof ReactNativeDeviceInfo | undefined = undefined
 
 try {
-  OptionalReactNativeDeviceInfo = require('react-native-device-info')
+  OptionalReactNativeDeviceInfo = require('react-native-device-info')  // No Web support
 } catch (e) {}
