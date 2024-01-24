c14391c54435b035a4ab38aa5d05d62ea5174aa6
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2024-01-24 14:19:46 +0100

    chore: React Native for macOS support (#162)

--- posthog-react-native/src/optional/OptionalExpoLocalization.ts
+++ posthog-react-native/src/optional/OptionalExpoLocalization.ts
@@ -1,7 +1,11 @@
 import type ExpoLocalization from 'expo-localization'
+import { Platform } from 'react-native'
 
 export let OptionalExpoLocalization: typeof ExpoLocalization | undefined = undefined
 
 try {
-  OptionalExpoLocalization = require('expo-localization')
+  // macos not supported
+  if (Platform.OS !== 'macos') {
+    OptionalExpoLocalization = require('expo-localization')
+  }
 } catch (e) {}
