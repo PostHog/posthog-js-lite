29414573446812d574f870115743b14e7b09624b
Manoel Aranda Neto <marandaneto@gmail.com>
2024-02-15 14:37:26 +0100

    prettier

--- examples/example_expo_rn_web/App.tsx
+++ examples/example_expo_rn_web/App.tsx
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
-import PostHog from 'posthog-react-native';
+import PostHog from 'posthog-react-native'
 
-export const $posthog = PostHog.initAsync(
-  'phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCqlL4ayELj4X8',
-  {
-    host: 'https://app.posthog.com',
-    flushAt: 1,
-    captureNativeAppLifecycleEvents: false,
-    sendFeatureFlagEvent: false,
-    preloadFeatureFlags: false,
-    // captureMode: 'form',
-    // persistence: 'memory',
-  },
-).then(client => {
-  client.debug(true);
+export const $posthog = PostHog.initAsync('phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCqlL4ayELj4X8', {
+  host: 'https://app.posthog.com',
+  flushAt: 1,
+  captureNativeAppLifecycleEvents: false,
+  sendFeatureFlagEvent: false,
+  preloadFeatureFlags: false,
+  // captureMode: 'form',
+  // persistence: 'memory',
+}).then((client) => {
+  client.debug(true)
   // console.log('PostHog client initialized');
-  client.capture('test');
+  client.capture('test')
   // console.log(`test: ${client.getFeatureFlag('test')}`);
   // client.on('error', err => {
   //   console.error('PostHog error', err);
@@ -25,7 +22,7 @@ export const $posthog = PostHog.initAsync(
   // client.on('capture', ev => {
   //   console.error('PostHog ev', ev);
   // });
-});
+})
 
 export default function App() {
   return (
@@ -33,7 +30,7 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -43,4 +40,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
