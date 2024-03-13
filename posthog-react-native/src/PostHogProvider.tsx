PostHog/posthog-js-lite

commit a1d9091396682de005f448d11531c0425e0b18d5

feat: Refactor the SDK to support a lazy initialization (#189)

posthog-react-native/src/PostHogProvider.tsx
@@ -1,17 +1,17 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo } from 'react'
 import { GestureResponderEvent, StyleProp, View, ViewStyle } from 'react-native'
 import { PostHog, PostHogOptions } from './posthog-rn'
 import { autocaptureFromTouchEvent } from './autocapture'
 import { useNavigationTracker } from './hooks/useNavigationTracker'
 import { useLifecycleTracker } from './hooks/useLifecycleTracker'
-import { PostHogContext } from './PosthogContext'
+import { PostHogContext } from './PostHogContext'
 import { PostHogAutocaptureOptions } from './types'
 
 export interface PostHogProviderProps {
   children: React.ReactNode
   options?: PostHogOptions
   apiKey?: string
-  client?: PostHog | Promise<PostHog>
+  client?: PostHog
   autocapture?: boolean | PostHogAutocaptureOptions
   debug?: boolean
   style?: StyleProp<ViewStyle>
@@ -33,12 +33,6 @@ function PostHogLifecycleHook({ client }: { client?: PostHog }): JSX.Element | n
   return null
 }
 
-const isPromise = (value: any): value is Promise<PostHog> => {
-  // NOTE: We don't use `instanceof Promise` because we found that some SDKs patch the global Promise object
-  // See https://github.com/PostHog/posthog-js-lite/issues/182
-  return value && typeof value.then === 'function'
-}
-
 export const PostHogProvider = ({
   children,
   client,
@@ -48,8 +42,22 @@ export const PostHogProvider = ({
   style,
   debug = false,
 }: PostHogProviderProps): JSX.Element | null => {
-  // Check if the client is a promise and resolve it if so
-  const [posthog, setPosthog] = useState<PostHog | undefined>(isPromise(client) ? undefined : client)
+  if (!client || !apiKey) {
+    throw new Error(
+      'Either a PostHog client or an apiKey is required. If want to use the PostHogProvider without a client, please provide an apiKey and the options={ disabled: true }.'
+    )
+  }
+
+  const posthog = useMemo(() => {
+    if (client && apiKey) {
+      console.warn(
+        'You have provided both a client and an apiKey to PostHogProvider. The apiKey will be ignored in favour of the client.'
+      )
+    }
+
+    return client ?? new PostHog(apiKey, options)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [client, apiKey])
 
   const autocaptureOptions = useMemo(
     () => (autocapture && typeof autocapture !== 'boolean' ? autocapture : {}),
@@ -63,30 +71,7 @@ export const PostHogProvider = ({
   const captureLifecycle =
     !captureNone && posthog && (captureAll || (autocaptureOptions?.captureLifecycleEvents ?? true)) // Default to true if not set
 
-  // Resolve async client to concrete client
   useEffect(() => {
-    if (client && apiKey) {
-      console.warn(
-        'You have provided both a client and an apiKey to PostHogProvider. The apiKey will be ignored in favour of the client.'
-      )
-    }
-
-    if (!posthog && client) {
-      if (isPromise(client)) {
-        client.then(setPosthog)
-      } else {
-        setPosthog(client)
-      }
-    } else if (!posthog && apiKey) {
-      PostHog.initAsync(apiKey, options).then(setPosthog)
-    }
-  }, [client, apiKey, posthog, options])
-
-  useEffect(() => {
-    if (!posthog) {
-      return
-    }
-
     posthog.debug(debug)
   }, [debug, posthog])
 

examples/example-expo/e2e/__snapshots__/posthog.e2e.ts.snap
@@ -35,7 +35,7 @@ Array [
     "tag_name": "ScrollView",
   },
   Object {
-    "tag_name": "PosthogDemoScreen",
+    "tag_name": "PostHogDemoScreen",
   },
   Object {
     "attr__name": "TabOne",

examples/example-expo/navigation/index.tsx
@@ -14,8 +14,8 @@ import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../typ
 import LinkingConfiguration from './LinkingConfiguration'
 
 import { PostHogProvider } from 'posthog-react-native'
-import PostHogDebugScreen, { usePosthogDebugEvents } from '../screens/PostHogDebugScreen'
-import PosthogDemoScreen from '../screens/PosthogDemoScreen'
+import PostHogDebugScreen, { usePostHogDebugEvents } from '../screens/PostHogDebugScreen'
+import PostHogDemoScreen from '../screens/PostHogDemoScreen'
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -44,14 +44,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 
 function RootNavigator() {
   // NOTE: This is a debugging hook just for this example
-  usePosthogDebugEvents()
+  usePostHogDebugEvents()
   return (
     <Stack.Navigator>
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
       <Stack.Group screenOptions={{ presentation: 'modal' }}>
         <Stack.Group>
-          <Stack.Screen name="Modal" component={PosthogDemoScreen} />
-          <Stack.Screen name="ModalNextPage" component={PosthogDemoScreen} />
+          <Stack.Screen name="Modal" component={PostHogDemoScreen} />
+          <Stack.Screen name="ModalNextPage" component={PostHogDemoScreen} />
         </Stack.Group>
       </Stack.Group>
     </Stack.Navigator>
@@ -69,7 +69,7 @@ function BottomTabNavigator() {
     <BottomTab.Navigator initialRouteName="TabOne">
       <BottomTab.Screen
         name="TabOne"
-        component={PosthogDemoScreen}
+        component={PostHogDemoScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
           title: 'Tab One',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
@@ -88,7 +88,7 @@ function BottomTabNavigator() {
       />
       <BottomTab.Screen
         name="TabTwo"
-        component={PosthogDemoScreen}
+        component={PostHogDemoScreen}
         options={{
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,

examples/example-expo/screens/PostHogDebugScreen.tsx
@@ -5,12 +5,11 @@ import { Alert, FlatList, StyleSheet, TouchableOpacity, View, Text } from 'react
 // NOTE: This would obviously need to be something like Redux in production
 export const GLOBAL_EVENTS: { event: string; payload: any }[] = []
 
-export const usePosthogDebugEvents = () => {
+export const usePostHogDebugEvents = () => {
   const posthog = usePostHog()
   const [localEvents, setLocalEvents] = useState(GLOBAL_EVENTS)
 
   useEffect(() => {
-    if (!posthog) return
     const onEvent = (event: string, payload: any) => {
       // console.log('On event', event, payload)
       GLOBAL_EVENTS.push({
@@ -38,7 +37,7 @@ export const usePosthogDebugEvents = () => {
 }
 
 export default function PostHogDebugScreen() {
-  const localEvents = usePosthogDebugEvents()
+  const localEvents = usePostHogDebugEvents()
   return (
     <View style={styles.container}>
       <View style={styles.header}>

examples/example-expo/screens/PostHogDemoScreen.tsx
@@ -3,21 +3,21 @@ import { useFeatureFlags, usePostHog } from 'posthog-react-native'
 import React from 'react'
 import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from 'react-native'
 
-export default function PosthogDemoScreen(props: any) {
+export default function PostHogDemoScreen(props: any) {
   const navigation = useNavigation()
   const posthog = usePostHog()
 
   const title = props.route.name
 
   const trackRandomEvent = () => {
-    posthog?.capture('random event', {
+    posthog.capture('random event', {
       random: Math.random(),
     })
   }
 
   const identifyUser = () => {
     const id = Math.round(Math.random() * 1000)
-    posthog?.identify(`user-${id}`, {
+    posthog.identify(`user-${id}`, {
       email: `user-${id}@posthog.com`,
     })
   }

examples/example-web/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { posthog } from './posthog'
 
 const GLOBAL_EVENTS: { event: string; payload: any }[] = []
-export const usePosthogDebugEvents = () => {
+export const usePostHogDebugEvents = () => {
   const [localEvents, setLocalEvents] = useState(GLOBAL_EVENTS)
 
   useEffect(() => {
@@ -34,7 +34,7 @@ export const usePosthogDebugEvents = () => {
 }
 
 const DebugEvents = (): JSX.Element => {
-  const events = usePosthogDebugEvents()
+  const events = usePostHogDebugEvents()
 
   return (
     <div className="Debugger">

examples/example_expo_rn_web/App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 
 import PostHog from 'posthog-react-native'
 
-export const $posthog = PostHog.initAsync('phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCqlL4ayELj4X8', {
+export const posthog = new PostHog('phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCqlL4ayELj4X8', {
   host: 'https://app.posthog.com',
   flushAt: 1,
   captureNativeAppLifecycleEvents: false,
@@ -11,18 +11,9 @@ export const $posthog = PostHog.initAsync('phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCql
   preloadFeatureFlags: false,
   // captureMode: 'form',
   // persistence: 'memory',
-}).then((client) => {
-  client.debug(true)
-  // console.log('PostHog client initialized');
-  client.capture('test')
-  // console.log(`test: ${client.getFeatureFlag('test')}`);
-  // client.on('error', err => {
-  //   console.error('PostHog error', err);
-  // });
-  // client.on('capture', ev => {
-  //   console.error('PostHog ev', ev);
-  // });
 })
+posthog.debug(true)
+posthog.capture('test')
 
 export default function App() {
   return (

examples/example_rn/posthog.tsx
@@ -1,7 +1,7 @@
 import PostHog, {PostHogProvider} from 'posthog-react-native';
 import React from 'react';
 
-export const $posthog = PostHog.initAsync(
+export const posthog = new PostHog(
   'phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCqlL4ayELj4X8',
   {
     host: 'https://app.posthog.com',
@@ -12,7 +12,7 @@ export const $posthog = PostHog.initAsync(
 export const SharedPostHogProvider = (props: any) => {
   return (
     <PostHogProvider
-      client={$posthog}
+      client={posthog}
       autocapture={{
         captureLifecycleEvents: true,
         captureScreens: true,

commit 62cd8a3a35d703c9bed4c6aef3316351a90afa80

fix: PostHogProvider initialization logic
A regression introduced by #189 requires a client and apiKey to
intialize a PostHogProvider even though the error message says only one
is required.

The error condition logic is fixed and a non null assertion is made.

posthog-react-native/src/PostHogProvider.tsx
@@ -42,7 +42,7 @@ export const PostHogProvider = ({
   style,
   debug = false,
 }: PostHogProviderProps): JSX.Element | null => {
-  if (!client || !apiKey) {
+  if (!client && !apiKey) {
     throw new Error(
       'Either a PostHog client or an apiKey is required. If want to use the PostHogProvider without a client, please provide an apiKey and the options={ disabled: true }.'
     )
@@ -55,7 +55,7 @@ export const PostHogProvider = ({
       )
     }
 
-    return client ?? new PostHog(apiKey, options)
+    return client ?? new PostHog(apiKey!, options)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [client, apiKey])
 