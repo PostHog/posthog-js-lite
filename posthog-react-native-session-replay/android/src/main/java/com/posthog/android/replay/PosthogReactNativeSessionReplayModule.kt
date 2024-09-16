f77ab1ec47f94eaed959df22d86b6800fc32b6de
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-16 15:04:09 +0100

    fixes

--- posthog-react-native-session-replay/android/src/main/java/com/posthog/android/replay/PosthogReactNativeSessionReplayModule.kt
+++ posthog-react-native-session-replay/android/src/main/java/com/posthog/android/replay/PosthogReactNativeSessionReplayModule.kt
@@ -1,4 +1,4 @@
-package com.posthogreactnativesessionreplay
+package com.posthog.android.replay
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
--- posthog-react-native-session-replay/android/src/main/java/com/posthog/android/replay/PosthogReactNativeSessionReplayPackage.kt
+++ posthog-react-native-session-replay/android/src/main/java/com/posthog/android/replay/PosthogReactNativeSessionReplayPackage.kt
@@ -1,4 +1,4 @@
-package com.posthogreactnativesessionreplay
+package com.posthog.android.replay
 
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
