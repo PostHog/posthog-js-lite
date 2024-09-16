21641b675290f2e39460af082c951e1f2043a4e2
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-16 15:59:53 +0100

    fix

--- posthog-react-native-session-replay/android/src/main/java/com/posthog/android/replay/PostHogReactNativeSessionReplayPackage.kt
+++ posthog-react-native-session-replay/android/src/main/java/com/posthog/android/replay/PostHogReactNativeSessionReplayPackage.kt
@@ -8,7 +8,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class PostHogReactNativeSessionReplayPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return listOf(PostHogReactNativeSessionReplay(reactContext))
+    return listOf(PostHogReactNativeSessionReplayModule(reactContext))
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
