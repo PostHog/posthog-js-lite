aae45fca3cbbb2cce574faa08285f1a779ab24a8
dylan <dylan@posthog.com>
2025-01-14 12:46:45 -0800

    formatting

--- posthog-node/src/feature-flags.ts
+++ posthog-node/src/feature-flags.ts
@@ -458,7 +458,7 @@ class FeatureFlagsPoller {
 // # uniformly distributed between 0 and 1, so if we want to show this feature to 20% of traffic
 // # we can do _hash(key, distinct_id) < 0.2
 function _hash(key: string, distinctId: string, salt: string = ''): number {
-  const sha1Hash = createHash("sha1")
+  const sha1Hash = createHash('sha1')
   sha1Hash.update(`${key}.${distinctId}${salt}`)
   return parseInt(sha1Hash.digest('hex').slice(0, 15), 16) / LONG_SCALE
 }
