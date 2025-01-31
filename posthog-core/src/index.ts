c76854015f38dd3adc17cb7517865ed0b7f92c41
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-01-31 16:03:02 +0100

    fix: feedback field

--- posthog-core/src/index.ts
+++ posthog-core/src/index.ts
@@ -1432,7 +1432,7 @@ export abstract class PostHogCore extends PostHogCoreStateless {
    */
   captureTraceFeedback(traceId: string | number, userFeedback: string): void {
     this.capture('$ai_feedback', {
-      $ai_feedback: userFeedback,
+      $ai_feedback_text: userFeedback,
       $ai_trace_id: String(traceId),
     })
   }
