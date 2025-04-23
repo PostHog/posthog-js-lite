fe8a67c9f1296590ea526bcad6a07fbbe9b6ee79
David Newell <david@posthog.com>
2025-04-23 14:08:47 +0100

    types

--- posthog-react-native/babel.config.js
+++ posthog-react-native/babel.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/env', '@babel/preset-typescript'],
+  presets: ['module:metro-react-native-babel-preset', '@babel/env'],
 }
