463373d75f5b578d41ca6888f8409957125bb44b
Peter Kirkham <peter@posthog.com>
2025-04-28 21:31:20 -0700

    fix: prettier

--- posthog-ai/src/utils.ts
+++ posthog-ai/src/utils.ts
@@ -240,9 +240,15 @@ export const sendEventToPosthog = ({
       console.log('Sending event to PostHog', JSON.stringify(properties))
       try {
         // @ts-ignore
-        console.log('Size of properties (kb)', Math.round(Buffer.byteLength(JSON.stringify(properties), STRING_FORMAT) / 1024 * 10000) / 10000)
+        console.log(
+          'Size of properties (kb)',
+          Math.round((Buffer.byteLength(JSON.stringify(properties), STRING_FORMAT) / 1024) * 10000) / 10000
+        )
         // @ts-ignore
-        console.log('Size of properties (mb)', Math.round(Buffer.byteLength(JSON.stringify(properties), STRING_FORMAT) / 1024 / 1024 * 10000) / 10000)
+        console.log(
+          'Size of properties (mb)',
+          Math.round((Buffer.byteLength(JSON.stringify(properties), STRING_FORMAT) / 1024 / 1024) * 10000) / 10000
+        )
       } catch (error) {
         console.error('Error printing size of properties', error)
       }
