b7a96d74166b9b52181693fea3fcc174887f208a
Oliver Browne <oliver@posthog.com>
2025-04-30 13:56:43 +0300

    update comments

--- posthog-node/src/types.ts
+++ posthog-node/src/types.ts
@@ -80,7 +80,7 @@ export type PostHogNodeV1 = {
   capture({ distinctId, event, properties, groups, sendFeatureFlags }: EventMessage): void
 
   /**
-   * @description Capture an event immediately. This is useful for events that should be captured immediately, such as a user logging in.
+   * @description Capture an event immediately. Useful for edge environments where the usual queue-based sending is not preferable. Do not mix immediate and non-immediate calls.
    * @param distinctId which uniquely identifies your user
    * @param event We recommend using [verb] [noun], like movie played or movie updated to easily identify what your events mean later on.
    * @param properties OPTIONAL | which can be a object with any information you'd like to add
@@ -99,9 +99,8 @@ export type PostHogNodeV1 = {
   identify({ distinctId, properties }: IdentifyMessage): void
 
   /**
-   * @description Identify lets you add metadata on your users so you can more easily identify who they are in PostHog,
-   * and even do things like segment users by these properties.
-   * An identify call requires:
+   * @description Identify lets you add metadata on your users so you can more easily identify who they are in PostHog.
+   * Useful for edge environments where the usual queue-based sending is not preferable. Do not mix immediate and non-immediate calls.
    * @param distinctId which uniquely identifies your user
    * @param properties with a dict with any key: value pairs
    */
@@ -122,12 +121,7 @@ export type PostHogNodeV1 = {
 
   /**
    * @description To marry up whatever a user does before they sign up or log in with what they do after you need to make an alias call.
-   * This will allow you to answer questions like "Which marketing channels leads to users churning after a month?"
-   * or "What do users do on our website before signing up?"
-   * In a purely back-end implementation, this means whenever an anonymous user does something, you'll want to send a session ID with the capture call.
-   * Then, when that users signs up, you want to do an alias call with the session ID and the newly created user ID.
-   * The same concept applies for when a user logs in. If you're using PostHog in the front-end and back-end,
-   *  doing the identify call in the frontend will be enough.:
+   * Useful for edge environments where the usual queue-based sending is not preferable. Do not mix immediate and non-immediate calls.
    * @param distinctId the current unique id
    * @param alias the unique ID of the user before
    */
