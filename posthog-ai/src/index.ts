9147f47a729f7794eb5bf72f9b623d6d1c8f0daf
Peter Kirkham <peter@posthog.com>
2025-05-28 16:03:04 -0700

    feat: gemini

--- posthog-ai/src/index.ts
+++ posthog-ai/src/index.ts
@@ -2,10 +2,12 @@ import PostHogOpenAI from './openai'
 import PostHogAzureOpenAI from './openai/azure'
 import { wrapVercelLanguageModel } from './vercel/middleware'
 import PostHogAnthropic from './anthropic'
+import PostHogGemini from './gemini'
 import { LangChainCallbackHandler } from './langchain/callbacks'
 
 export { PostHogOpenAI as OpenAI }
 export { PostHogAzureOpenAI as AzureOpenAI }
 export { PostHogAnthropic as Anthropic }
+export { PostHogGemini as Gemini }
 export { wrapVercelLanguageModel as withTracing }
 export { LangChainCallbackHandler }
